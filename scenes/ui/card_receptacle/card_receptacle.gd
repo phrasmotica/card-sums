6de608d843d1165fa3ebc52958a49810e2a24b27
Julian Lawrance <julian.lawrance@gmail.com>
2025-08-13 09:08:41 +0100

    Add state factory innards to CardReceptacle script

--- scenes/ui/card_receptacle/card_receptacle.gd
+++ scenes/ui/card_receptacle/card_receptacle.gd
@@ -2,3 +2,24 @@ class_name CardReceptacle
 extends Node
 
 enum State { INACTIVE, WAITING, ACCEPTED }
+
+var _state_factory := CardReceptacleStateFactory.new()
+var _current_state: CardReceptacleState = null
+
+func _ready() -> void:
+	switch_state(CardReceptacle.State.INACTIVE)
+
+func switch_state(state: State, state_data := CardReceptacleStateData.new()) -> void:
+	if _current_state != null:
+		_current_state.queue_free()
+
+	_current_state = _state_factory.get_fresh_state(state)
+
+	_current_state.setup(
+		self,
+		state_data)
+
+	_current_state.state_transition_requested.connect(switch_state)
+	_current_state.name = "CardReceptacleStateMachine: %s" % str(state)
+
+	call_deferred("add_child", _current_state)
