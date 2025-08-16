1aec25084cc6b2dd603aeb9a46a3cef1668027ae
Julian L <julian.lawrance@gmail.com>
2025-08-16 17:38:32 +0100

    Allow hovering over a receptacle's captured card

--- scenes/ui/card_receptacle/states/card_receptacle_state_accepted.gd
+++ scenes/ui/card_receptacle/states/card_receptacle_state_accepted.gd
@@ -16,3 +16,20 @@ func _capture_card() -> void:
 		card.reparent(_card_receptacle)
 		card.position = _card_receptacle.size / 2.0
 		card.rotation = 0.0
+
+		card.enable()
+
+		SignalHelper.persist(card.hovered, _on_hovered.bind(card))
+		SignalHelper.persist(card.unhovered, _on_unhovered.bind(card))
+
+		# do this after the previous state has been freed
+		SignalHelper.once_next_frame(
+			func() -> void:
+				_card_receptacle.mouse_filter = Control.MOUSE_FILTER_IGNORE
+		)
+
+func _on_hovered(card: Card) -> void:
+	card.activate()
+
+func _on_unhovered(card: Card) -> void:
+	card.deactivate()
