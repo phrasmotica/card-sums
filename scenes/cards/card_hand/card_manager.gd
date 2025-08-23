cd9418f79f879bc1d87e894b604bb40afe901250
Julian L <julian.lawrance@gmail.com>
2025-08-23 19:13:11 +0100

    Fix bug in releasing a captured card from a manager

--- scenes/cards/card_hand/card_manager.gd
+++ scenes/cards/card_hand/card_manager.gd
@@ -15,12 +15,12 @@ func _ready() -> void:
 		return
 
 	for c in renderer.cards:
-		SignalHelper.persist(c.captured, _on_captured.bind(c))
+		SignalHelper.once(c.captured, _release_card.bind(c))
 		SignalHelper.persist(c.dragged, _on_dragged.bind(c))
 		SignalHelper.persist(c.hovered, _on_hovered.bind(c))
 		SignalHelper.persist(c.unhovered, _on_unhovered.bind(c))
 
-func _on_captured(card: Card) -> void:
+func _release_card(card: Card) -> void:
 	renderer.remove_card(card)
 
 	card.dragged.disconnect(_on_dragged.bind(card))
